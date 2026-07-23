579c6c04f4a44c808b0f8d0f9e15c35c38984ca2
root <root@openclaw-server.bcottage>
2026-07-23 01:43:57 +0000

    DevSecurity-1 iter 101: Add is_string guard for ['wac_agent'] in admin_notices

--- includes/class-core.php
+++ includes/class-core.php
@@ -206,7 +206,7 @@ class Core {
         }
 
         $msg    = ( isset( $_GET['wac_msg'] ) && is_string( $_GET['wac_msg'] ) ) ? sanitize_key( wp_unslash( $_GET['wac_msg'] ) ) : '';
-        $agent  = isset( $_GET['wac_agent'] ) ? sanitize_key( wp_unslash( $_GET['wac_agent'] ) ) : '';
+        $agent  = ( isset( $_GET['wac_agent'] ) && is_string( $_GET['wac_agent'] ) ) ? sanitize_key( wp_unslash( $_GET['wac_agent'] ) ) : '';
         // phpcs:enable WordPress.Security.NonceVerification.Recommended
 
         $notices = array(
